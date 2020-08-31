1833cbadd9fea84c8f3faf7566e6bdc0f38c1aa5
henrispkl <henriqueirineu10@hotmail.com>
2020-08-31 01:41:23 -0300

    Definitive fix for client request errors

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:4000/api/',
+  baseURL: '/api/',
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
